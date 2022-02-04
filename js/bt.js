34dd7af2847fdeb5030ebfcb25cafe7b516be936
maxi-q <maximkabust@gmail.com>
2022-02-04 21:18:14 +0300

    подпилил кнопки задизайнерил index

--- js/bt.js
+++ js/bt.js
@@ -1,24 +1,13 @@
-var B1 = document.getElementById('1');
-var B2 = document.getElementById('2');
-var B3 = document.getElementById('3');
-var B4 = document.getElementById('4');
-var B5 = document.getElementById('5');
-var B6 = document.getElementById('6');
-var B7 = document.getElementById('7');
-var B8 = document.getElementById('8');
-var B9 = document.getElementById('9');
-
-const buttons = [B1,B2,B3,
-                B4,B5,B6,
-                B7,B8,B9]
-
-listic = ["./tree.html", "./paper.html", "./energy.html",
+listic = ["./paper.html", "./energy.html",
 "./animals.html","./area.html","./factories.html",
-"./ffish.html","./toxiRain.html","./sea.html"]
+"./ffish.html","./tree.html","./sea.html"]
 
 document.addEventListener('click', function(event){
     element = event.target;
     if (element.classList.contains("mainBT")){
+        window.open(element.id);
+    }
+    else if (element.classList.contains("SuperBT")){
         window.open(shuffle(listic));
     }
 })
